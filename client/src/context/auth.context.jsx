001c8b312c41f50eeb684b2bbc2bd9710985498a
Wiler Marinez <wmarinez-e@sice-outsourcing.com>
2022-12-06 07:02:29 +0100

    removing get cookie function

--- client/src/context/auth.context.jsx
+++ client/src/context/auth.context.jsx
@@ -1,15 +1,5 @@
 import React, { createContext, useContext, useState } from 'react'
 
-export const getCookie = (name) => {
-  const cookieArr = document.cookie.split(";")
-  const cookie = cookieArr
-    .map((cookie) => {
-      return cookie.split('=')
-    })
-    .filter(([coockieName]) => coockieName.trim() === name)
-  return cookie.length ? cookie[0][1] : null
-}
-
 const AuthContext = createContext()
 
 export const ContextAuthProvider = ({ children }) => {
