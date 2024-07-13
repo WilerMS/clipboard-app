d505a081f4d3dde26d0bc70e5067709b28b1a92b
WilerMS <wilersoriano@gmail.com>
2024-07-13 13:33:41 +0200

    delete console logs

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,14 +1,8 @@
 // import { createPool } from 'mysql2/promise'
-import { dbConfig, dbTurso } from './../config.js'
+import { dbTurso } from './../config.js'
 import { createClient } from "@libsql/client";
-
-// export const pool = createPool(dbConfig)
-
 
 export const turso = createClient({
   url: dbTurso.url,
   authToken: dbTurso.token,
 });
- 
-const { rows: result } = await turso.execute("SELECT * FROM notes")
-console.log({result})
