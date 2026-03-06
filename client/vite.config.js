fae39021674a037e0278977c0536bd404f1f16e4
Wiler Mariñez <wilersoriano@gmail.com>
2026-03-07 00:22:23 +0100

    Refactor vite.config.js for consistency in string quotes and update base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
     define: {
-      'process.env': {},
+      "process.env": {},
     },
     build: {
-      outDir: 'build',
+      outDir: "build",
     },
     plugins: [react()],
-    base: '/clipboard-app'
+    base: "/",
   };
 });
