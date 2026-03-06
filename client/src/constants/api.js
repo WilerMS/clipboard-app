6b5545af54b3ea25cda5f23e34ad264290943575
Wiler Mariñez <wilersoriano@gmail.com>
2026-03-07 00:26:24 +0100

    Update API_HOST to point to the correct backend URL

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,1 +1,2 @@
-export const API_HOST = process.env.API_HOST || 'https://clipboard-app-production.up.railway.app'
+export const API_HOST =
+  process.env.API_HOST || "https://bulles-backend.up.railway.app";
